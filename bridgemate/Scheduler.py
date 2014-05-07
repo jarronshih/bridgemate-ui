93cc9da756b4277165991b6c6f1b242bf5b266df
Jarron Shih <jarronshih@gmail.com>
2014-05-08 00:42:17 +0800

    swiss scheduler frame

--- bridgemate/Scheduler.py
+++ bridgemate/Scheduler.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 class BaseScheduler(object):
     def __init__(self, meta_data):
@@ -20,7 +22,7 @@ class BaseScheduler(object):
 
 class CustomScheduler(BaseScheduler):
     def __init__(self, meta_data):
-        self.match = meta_data
+        self.match = meta_data["match"]
 
     def schedule_round(self, round_number):
         pass
@@ -29,22 +31,22 @@ class CustomScheduler(BaseScheduler):
         return self.match[round_number-1]
 
     def get_metadata(self):
-        return self.match
+        return {"match":self.match}
 
-class RoundRobinScheduler(BaseScheduler):
-    def __init__(self, meta_data):
-        self.meta_data = meta_data
-        self.team_count = meta_data['team_count']
+# class RoundRobinScheduler(BaseScheduler):
+#     def __init__(self, meta_data):
+#         self.meta_data = meta_data
+#         self.team_count = meta_data['team_count']
 
-    def schedule_round(self, round_number):
-        pass
+#     def schedule_round(self, round_number):
+#         pass
 
-    def get_match_by_round(self, current_round):
-        # return (tableid, ns_team, ew_team) array
-        raise NotImplementedError
+#     def get_match_by_round(self, current_round):
+#         # return (tableid, ns_team, ew_team) array
+#         raise NotImplementedError
 
-    def get_metadata(self):
-        return self.meta_data
+#     def get_metadata(self):
+#         return self.meta_data
 
 #     def schedule_next_round(self):
 #         self.current_round = self.current_round + 1
@@ -58,6 +60,58 @@ class RoundRobinScheduler(BaseScheduler):
 #             self._round_robin_arry = next_array
 
 
-# class SwissScheduler(BaseScheduler):
-#     def __init__(self, team_count):
-#         super(SwissSchedule, self).__init__(team_count)
+class SwissScheduler(BaseScheduler):
+    def __init__(self, meta_data):
+        self.match = meta_data["match"]
+        self.score = meta_data["score"]
+
+    def schedule_round(self, round_number):
+        raise NotImplementedError
+
+    def get_match_by_round(self, current_round):
+        # return (tableid, ns_team, ew_team) array
+        return self.match[current_round-1]
+
+    def get_metadata(self):
+        return {
+            "match": self.match,
+            "score": self.score,
+        }
+
+
+# Unittest
+
+
+class CustomSchedulerTest(unittest.TestCase):
+    def test_custom_format(self):
+        print "test_custom_format"
+
+        meta_data = {
+            "team_count"
+        }
+
+class SwissSchedulerTest(unittest.TestCase):
+    def test_16team_swiss_5round(self):
+        print "test_16team_swiss_5round"
+        team_count = 16
+        round_count = 5
+        meta_data = {
+            "match": [],
+            "score": [ (x+1,0) for x in xrange(team_count) ]
+        }
+        swiss = SwissScheduler(meta_data)
+
+        for r in xrange(round_count):
+            current_round = r+1
+            print "Schedule round: %d" % current_round 
+            swiss.schedule_round(current_round)
+            match = swiss.get_match_by_round(current_round)
+
+            for table, ns_team, ew_team in match:
+                print "Table %d: NS=%d EW=%d" % (table, ns_team, ew_team)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
--- unit_test.py
+++ unit_test.py
@@ -7,27 +7,30 @@ def test_run_custom():
                             team_count=2, 
                             scheduler_type="CustomScheduler",
                             scheduler_metadata=
-                            [
-                                [ (1, 1, 2), (2, 2, 1) ]  # Round 1 (table_id, ns_team, ew_team)
-                            ],
+                            {
+                                "match":
+                                [
+                                    [ (1, 1, 2), (2, 2, 1) ]  # Round 1 (table_id, ns_team, ew_team)
+                                ]
+                            }
                             total_round=1, 
                             board_count=2
                             )
     project.run()
 
 
-def test_run_roundrobin():
-    project = create_project('test_roundrobin')
-    project.setup_config(   tm_name='Round', 
-                            team_count=4, 
-                            scheduler_type="RoundRobinScheduler", 
-                            scheduler_metadata=
-                            {
-                                "team_count": 4,
-                            }
-                            total_round=4, 
-                            board_count=3
-                            )
-    project.run()
+# def test_run_roundrobin():
+#     project = create_project('test_roundrobin')
+#     project.setup_config(   tm_name='Round', 
+#                             team_count=4, 
+#                             scheduler_type="RoundRobinScheduler", 
+#                             scheduler_metadata=
+#                             {
+#                                 "team_count": 4,
+#                             }
+#                             total_round=4, 
+#                             board_count=3
+#                             )
+#     project.run()
 
 test_run_custom()
