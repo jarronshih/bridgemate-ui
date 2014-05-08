b19605a8cb3a70e594c9a42d0ced1d82497948c4
Jarron Shih <jarronshih@gmail.com>
2014-05-09 01:31:26 +0800

    gui, run btn, edit config btn

--- bridgemate/GUI.py
+++ bridgemate/GUI.py
@@ -5,22 +5,23 @@ from utils.config import PROJECT_FOLDER
 MAINFRAME_HEIGHT=400
 MAINFRAME_WIDTH=600
 
+ID_MAINFRAME=100
 
 class MainFrame(wx.Frame):
-    def __init__(self, parent=None, id=-1, title="Main"):
+    def __init__(self):
         super(MainFrame, self).__init__(
-            parent=parent, 
-            id=id, 
-            title=title,
+            parent=None, 
+            id=ID_MAINFRAME, 
             style= wx.CAPTION | wx.SYSTEM_MENU  | wx.MINIMIZE_BOX | wx.CLOSE_BOX | wx.STAY_ON_TOP,
             )
         self.bm2_manager = None
-        self.InitUI()
+        self.init_UI()
         self.SetSize((MAINFRAME_WIDTH, MAINFRAME_HEIGHT))
         self.Centre()
+        self.reload_project()
         
 
-    def InitUI(self):
+    def init_UI(self):
         # Top Menu
         menubar = wx.MenuBar()
         self.SetMenuBar(menubar)
@@ -33,17 +34,28 @@ class MainFrame(wx.Frame):
         file_menu.AppendSeparator()
         quit_item = file_menu.Append(wx.ID_EXIT, '&Quit\tCtrl+Q')
 
-        self.Bind(wx.EVT_MENU, self.OnNew, new_item)
-        self.Bind(wx.EVT_MENU, self.OnOpen, open_item)
-        self.Bind(wx.EVT_MENU, self.OnQuit, quit_item)
+        self.Bind(wx.EVT_MENU, self.on_new, new_item)
+        self.Bind(wx.EVT_MENU, self.on_open, open_item)
+        self.Bind(wx.EVT_MENU, self.on_quit, quit_item)
+
+        # Button
+        self.edit_config_btn = wx.Button(self, label='Edit Config', pos=(10,20) )
+        self.run_one_round_btn = wx.Button(self, label='Run one round', pos=(10,50) )
+
+        self.Bind(wx.wx.EVT_BUTTON, self.on_edit_config, self.edit_config_btn)
+        self.Bind(wx.wx.EVT_BUTTON, self.on_run_one_round, self.run_one_round_btn)
+
+        # Statusbar
+        self.statusbar = self.CreateStatusBar()
+        self.statusbar.SetStatusText('')
 
         #Other        
         
 
-    def OnQuit(self, e):
+    def on_quit(self, e):
         self.Close()
 
-    def OnNew(self, e):
+    def on_new(self, e):
         import datetime
         today_str = datetime.datetime.today().strftime("%Y_%m_%d")
         dlg = wx.TextEntryDialog(None,'Enter project name:','New Project', '%s_touronment' % today_str)
@@ -57,11 +69,11 @@ class MainFrame(wx.Frame):
                 err_dial.ShowModal()
                 err_dial.Destroy()
             else:
-                self.InitUI()
+                self.reload_project()
 
         dlg.Destroy()
         
-    def OnOpen(self, e):
+    def on_open(self, e):
         dlg = wx.DirDialog(self, "Choose a directory:",
                            style=wx.DD_DEFAULT_STYLE
                            )
@@ -69,9 +81,52 @@ class MainFrame(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             project_name = dlg.GetPath()
             self.bm2_manager = open_project(project_name)
-            self.InitUI()
+            self.reload_project()
         dlg.Destroy()
 
+    def on_edit_config(self, e):
+        if self.bm2_manager is None:
+            return
+
+        dlg = ProjectConfigFrame(parent=self,config=self.bm2_manager.config)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.reload_project()
+        dlg.Destroy()
+
+    def on_run_one_round(self, e):
+        self.bm2_manager.run_one_round()
+
+    def reload_project(self, status_string=''):
+        if self.bm2_manager is None:
+            self.statusbar.SetStatusText("Please Open / Create project")
+            self.SetTitle("Main")
+            self.edit_config_btn.Disable()
+            self.run_one_round_btn.Disable()
+
+        else:
+            self.statusbar.SetStatusText(status_string)
+            self.SetTitle(self.bm2_manager.config.project_name)
+            self.edit_config_btn.Enable()
+            self.run_one_round_btn.Enable()
+
+
+class ProjectConfigFrame(wx.Dialog):
+    def __init__(self, config, parent):
+        super(ProjectConfigFrame, self).__init__(parent=parent)
+        self.config = config
+        
+        self.Centre()
+        self.Show(True)
+
+    def on_init(self):
+        wx.StaticText(self, -1, 'Width:', (20, 20))
+        wx.StaticText(self, -1, 'Height:', (20, 70))
+        self.sc1 = wx.SpinCtrl(self, -1, str(w), (80, 15), (60, -1), min=200, max=500)
+        self.sc2 = wx.SpinCtrl(self, -1, str(h), (80, 65), (60, -1), min=200, max=500)
+
+
+        save_btn = wx.Button(self, 1, 'Save', (20, 120))
+        self.Bind(wx.EVT_BUTTON, self.OnSave, id=1)
 
 
 class MainApp(wx.App):
--- bridgemate/ProjectConfig.py
+++ bridgemate/ProjectConfig.py
@@ -9,7 +9,7 @@ class ProjectConfig(object):
         self.config_path = get_project_file_path(self.project_name, 'config.json')
 
         if os.path.exists(self.config_path):
-            self.load()
+            self.read()
         else:
             self.setup('TM', 0, "CustomScheduler", {"match":[]}, 0, 1, 0, 1, 1, 'A')
         
@@ -28,20 +28,28 @@ class ProjectConfig(object):
 
         self.write()
         
+    def dump(self):
+        return json.dumps(self.__dict__, indent=4)
+
+    def load(self, json_content):
+        self.__dict__ = json.loads(json_content)
+        
     def write(self):
-        json_content = json.dumps(self.__dict__, indent=4)
+        json_content = self.dump()
 
         f = open(self.config_path, 'w')
         f.write(json_content)
         f.close()
 
-    def load(self):
+    def read(self):
         # load config.json to config
         f = open(self.config_path)
         json_content = f.read()
         f.close()
 
-        self.__dict__ = json.loads(json_content)
+        self.load(json_content)
+
+
 
 
         
